a715961ba590fe76e1f1b6b4a6950033f922cac2
admin <admin@DESKTOP-NL59TQ3>
2023-07-21 16:01:03 +0530

    ...

--- src/javaProject/AYaz.java
+++ src/javaProject/AYaz.java
@@ -1,5 +1,21 @@
 package javaProject;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AYaz {
 
+	
+	
+	 public static void main(String[] args) throws IOException {
+		 Properties pop=new Properties();
+			pop.put("username", "ayaz@123");
+			pop.put("userPass", "ayaz1234");
+			pop.put("userAdress", "sakinaka");
+			FileOutputStream file=new FileOutputStream(System.getProperty("user.dir")+"/"+"siddiqui.properties");
+			pop.store(file, "siddiqui");
+			System.out.println(pop.get("username"));
+			
+	}
 }
